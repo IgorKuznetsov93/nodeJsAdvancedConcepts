14a3bb16cf19a3a204a0e2c24051347df2f2f520
Igor Kuznetsov <ik2903@inbox.ru>
2020-04-22 16:38:01 +0600

    add oauth2 strategy

--- BlogpostApp/config/dev.js
+++ BlogpostApp/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '1052631577481-lrihqt1cuuho13tphmkh38j1p4un25dg.apps.googleusercontent.com',
+  googleClientSecret: 'sApyP2e6HQ3TtzdZ4AqytZ-6',
+  mongoURI: 'mongodb://localhost:27017',
   cookieKey: '123123123'
 };
