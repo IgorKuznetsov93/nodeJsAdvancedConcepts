7f4ab78841333a32b65b335b73597f372c948b05
Igor Kuznetsov <ik2903@inbox.ru>
2020-04-25 23:30:08 +0600

    added redis cache

--- BlogpostApp/config/dev.js
+++ BlogpostApp/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  googleClientID:
+    googleClientID:
     '1052631577481-lrihqt1cuuho13tphmkh38j1p4un25dg.apps.googleusercontent.com',
-  googleClientSecret: 'sApyP2e6HQ3TtzdZ4AqytZ-6',
-  mongoURI: 'mongodb://localhost:27017',
-  cookieKey: '123123123'
+    googleClientSecret: 'sApyP2e6HQ3TtzdZ4AqytZ-6',
+    mongoURI: 'mongodb://localhost:27017',
+    cookieKey: '123123123',
 };
